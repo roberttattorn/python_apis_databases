ff03c2dad19fa73cac05900de915eaf040ca61b2
roberttattorn <73357517+roberttattorn@users.noreply.github.com>
2021-12-13 00:19:02 +0300

    Update Exercise_06.py

--- apis/Exercise_06.py
+++ apis/Exercise_06.py
@@ -15,3 +15,56 @@ It is your responsibility to build out the application to handle all menu option
 
 
 '''
+import requests
+from pprint import pprint
+code=input("Please select from the following options (enter the number of the action you'd like to take):\n
+1) Create a new account (POST)\n
+2) View all your tasks (GET)\n
+3) View your completed tasks (GET)\n
+4) View only your incomplete tasks (GET)\n
+5) Create a new task (POST)\n
+6) Update an existing task (PATCH/PUT)\n
+7) Delete a task (DELETE)\n")
+
+if code == 1:
+  name = input("Please input your name")
+  password = input("Please select your password")
+  data = {
+   "name":name,
+   "password":password
+  }
+  response = requests.post("http://demo.codingnomads.co:8080/tasks_api/users",data)
+  print(response.status_code)
+if code == 2:
+  response = requests.get("http://demo.codingnomads.co:8080/tasks_api/users?name=[name]&task=*")
+  data = response.json()
+  pprint(data)
+if code == 3:
+  response = requests.get("http://demo.codingnomads.co:8080/tasks_api/users?name=[name]&task!=''")
+  data = response.json()
+  pprint(data)
+if code == 4:
+  response = requests.get("http://demo.codingnomads.co:8080/tasks_api/users?name=[name]&task=''")
+  data = response.json()
+  pprint(data)
+if code == 5:
+  name = input("your name please")
+  task = input("create a task, please")
+  data = {
+    "name":name,
+   "task":task
+  }
+  response = requests.post("http://demo.codingnomads.co:8080/tasks_api/users",data)
+if code == 6:
+  name = input("your name please")
+  task = input("task to update")
+  data = {
+    "name":name,
+   "task":task
+  }
+  response = requests.put("http://demo.codingnomads.co:8080/tasks_api/users",data)
+  if code == 7:
+    user = input("user to delete")
+    requests.delete("http://demo.codingnomads.co:8080/tasks_api/users?name=user")
+  
+                           
