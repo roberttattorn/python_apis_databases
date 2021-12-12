4dc0dcc225f6ff08c87123309b225401ddfda52a
roberttattorn <73357517+roberttattorn@users.noreply.github.com>
2021-12-12 23:11:21 +0300

    Update Exercise_03.py

--- apis/Exercise_03.py
+++ apis/Exercise_03.py
@@ -8,3 +8,13 @@ and create a user with your information.
 Make a GET request to confirm that your user information has been saved.
 
 '''
+
+import requests
+
+user={
+    name:"Bob:,
+    email:"bob@mail.bs"
+}
+response = requests.post("http://demo.codingnomads.co:8080/tasks_api/users",user)
+check = requests.get("http://demo.codingnomads.co:8080/tasks_api/users?name=Bob&email=bob@mail.bs"
+print(check)
