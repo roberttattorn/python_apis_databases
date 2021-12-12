1dbc3e68e55578880b5118b1bfd0a05a7de3c518
roberttattorn <73357517+roberttattorn@users.noreply.github.com>
2021-12-12 23:17:51 +0300

    Update Exercise_04.py

--- apis/Exercise_04.py
+++ apis/Exercise_04.py
@@ -4,3 +4,14 @@ Write a program that makes a PUT request to update your user information to a ne
 Again make a GET request to confirm that your information has been updated.
 
 '''
+import requests
+from pprint import pprint
+data = {
+  first_name:"bob",
+  last_name:"Stress",
+  email:"bob@mail.bs"
+}
+response = put("http://demo.codingnomads.co:8080/tasks_api/users",data)
+check= get("http://demo.codingnomads.co:8080/tasks_api/users?first_name=Bob&last_name=Stress&email=bob@mail.bs")
+data = check.json()
+pprint(data)
